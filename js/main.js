968875fac55081dc5e8b38eff604e968921b8382
manishbupadhyay <ermanishupadhyay@gmail.com>
2018-09-29 00:44:07 +0530

    Add typed.js

--- js/main.js
+++ js/main.js
@@ -145,5 +145,37 @@
 		skillsWayPoint();
 	});
 
-
+	// Typed js 
+	var typed = new Typed('.typed', {
+		strings: ["Husband","Father of a baby boy","Front End Developer", "UI Developer"],
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 30,
+			// time before typing starts
+			startDelay: 1200,
+			// backspacing speed
+			backSpeed: 20,
+			// time before backspacing
+			backDelay: 500,
+			// loop
+			loop: true,
+			// false = infinite
+			loopCount: 5,
+			// show cursor
+			showCursor: false,
+			// character for cursor
+			cursorChar: "|",
+			// attribute to type (null == text)
+			attr: null,
+			// either html or text
+			contentType: 'html',
+			// call when done callback function
+			callback: function() {},
+			// starting callback function before each string
+			preStringTyped: function() {},
+			//callback for every typed string
+			onStringTyped: function() {},
+			// callback for reset
+			resetCallback: function() {}
+	  });
 }());
